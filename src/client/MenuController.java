e43efa05cf614c4c1cab5dcf903140c5e8145fce
sergiosegrera <sergioarturosegrera@gmail.com>
2020-12-20 16:24:01 -0500

    Add menu controller

---
+++ src/client/MenuController.java
@@ -0,0 +1,16 @@
+package client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MenuController {
+	@FXML
+	private TextField menuTextField;
+	
+    @FXML
+    private void addToMenu(ActionEvent event)
+    {
+        System.out.println("lollolol");
+    }
+}
--- src/module-info.java
+++ src/module-info.java
@@ -2,4 +2,7 @@ module CoffeeZone {
 	exports client;
 	requires javafx.graphics;
 	requires java.sql;
+	requires javafx.fxml;
+	requires javafx.controls;
+	requires javafx.base;
 }
